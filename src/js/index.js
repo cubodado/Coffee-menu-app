a3a578d6f58f5115289a7866130b23572e693973
cubodado <dalkang000@gmail.com>
2022-03-03 23:06:08 +0900

    Feat: create menuItemTemplate feature

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,29 @@ function App() {
     e.preventDefault();
   });
 
-  // 사용자 입력 이벤트 발생 시 입력값 콘솔로 출력하기
+  // 사용자 입력 이벤트 발생 시, 입력값으로 메뉴 템플릿 생성
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
-      console.log($("#espresso-menu-name").value);
+      const espressoMenuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = (name) => {
+        return `
+          <li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${name}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>
+        `;
+      };
     }
   });
 }
