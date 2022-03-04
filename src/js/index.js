8b8126f6c46c04cfb36464d14db5af1e5747f048
cubodado <dalkang000@gmail.com>
2022-03-04 16:22:13 +0900

    Refactor: shorten querySelector code

--- src/js/index.js
+++ src/js/index.js
@@ -67,9 +67,9 @@ function App() {
   // 수정 버튼 이벤트 발생 시, 입력값으로 메뉴 이름 변경
   $("#espresso-menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
-      const menuName = e.target.closest("li").querySelector(".menu-name").innerText;
-      const newMenuName = prompt("변경할 메뉴 이름을 입력해 주세요.", menuName);
-      e.target.closest("li").querySelector(".menu-name").innerText = newMenuName;
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const newMenuName = prompt("변경할 메뉴 이름을 입력해 주세요.", $menuName.innerText);
+      $menuName.innerText = newMenuName;
     }
   });
 }
