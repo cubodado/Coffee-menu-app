6ca578b767bfc57c43b85fae39c81566f8d87350
cubodado <dalkang000@gmail.com>
2022-03-06 23:51:56 +0900

    Refactor: remove espresso keyword

--- src/js/index.js
+++ src/js/index.js
@@ -46,27 +46,27 @@ function App() {
       `;
     }).join("");
 
-    $("#espresso-menu-list").innerHTML = menuItemTemplate;
+    $("#menu-list").innerHTML = menuItemTemplate;
     updateMenuCount();
   };
 
   const updateMenuCount = () => {
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length
+    const menuCount = $("#menu-list").querySelectorAll("li").length
     $(".menu-count").innerText = `총 ${menuCount} 개`;
   };
 
   const addMenuName = () => {
-    if ($("#espresso-menu-name").value === "") {
+    if ($("#menu-name").value === "") {
       alert("값을 입력해주세요");
       return;
     }
     
-    const espressoMenuName = $("#espresso-menu-name").value;
-    this.menu[this.currentCategoryName].push({ name: espressoMenuName });
+    const menuName = $("#menu-name").value;
+    this.menu[this.currentCategoryName].push({ name: menuName });
     store.setLocalStorage(this.menu);
     render();
 
-    $("#espresso-menu-name").value = "";
+    $("#menu-name").value = "";
   };
 
   const editMenuName = (e) => {
@@ -93,12 +93,12 @@ function App() {
   };
 
   // form 태그가 자동으로 전송되는 것 막아주기
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
+  $("#menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
   // 사용자 입력 이벤트 발생 시, 입력값으로 메뉴 템플릿 생성
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+  $("#menu-name").addEventListener("keypress", (e) => {
     // 엔터키가 아니면 return
     if (e.key !== "Enter") {
       return;
@@ -107,10 +107,10 @@ function App() {
   });
 
   // 확인 버튼 이벤트 발생 시, 입력값으로 메뉴 템플릿 생성
-  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
+  $("#menu-submit-button").addEventListener("click", addMenuName);
 
   // 이벤트 위임
-  $("#espresso-menu-list").addEventListener("click", (e) => {
+  $("#menu-list").addEventListener("click", (e) => {
     // 수정 버튼 이벤트 발생 시, 입력값으로 메뉴 이름 변경
     if (e.target.classList.contains("menu-edit-button")) {
       editMenuName(e);
